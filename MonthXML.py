24666b152a6cb5ef6427e04756a71770e9c2abf9
eLKresso <35309241+LKress@users.noreply.github.com>
2018-06-29 11:10:26 +0200

    Update MonthXML.py

--- MonthXML.py
+++ MonthXML.py
@@ -28,13 +28,13 @@ import time
 
 def addDay():
 	try:
-		et = etree.parse("XMLM.xml") #baum datei holen
+		et = etree.parse("XMLM.xml")
 		weatherArr = getDayData()
 		day = etree.SubElement(et.getroot(),"day"+time.strftime("%d"), temp=str(weatherArr[0]), hum=str(weatherArr[1]), pres=str(weatherArr[2])) #unterknoten mit aktuellem datum uebergeben
-		write(etree.ElementTree(et.getroot())) #baum schreiben
+		write(etree.ElementTree(et.getroot())) 
 	except etree.XMLSyntaxError:
 		sys.stderr.write("Error: File is empty\n")
-	except OSError: #fuer pi noch anpassen je nach fehlermeldung
+	except OSError: 
 		sys.stderr.write("Error: File could not be found\n")
 
 
